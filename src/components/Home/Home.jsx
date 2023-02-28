a98c502d15826959f69cefbc93e41f70d0107a2b
rusomalevich <ezequiel.glik@gmail.com>
2023-02-28 09:02:34 -0300

    trae imagenes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Header } from '../Header/Header'
 import { MuestraImagenes } from '../MuestraImagenes/MuestraImagenes'
+import './Home.css'
 
 const Home = ({ busqueda }) => {
 
+    const BASEurl = 'https://api.unsplash.com/';
+    const apiKey = 'kGS0Yw08xWCh64TiRy_U421UZRvGQhnmYWYuoIPWAs8'
+    const apiSearch = 'search/photos?query='
+    const apiRandom = 'photos/random'
+    const cantidadFotos = '5'
+    const limit = 'count='
+
+    const [imagenes, setImagenes] = useState([])
+    const [cargando, setCargando] = useState(false)
+
+    useEffect(() => {
+        traeImagenes()
+    }, [])
+
+    const traeImagenes = async () => {
+        setCargando(true)
+        const response = await axios.get(`${BASEurl}${apiRandom}?client_id=${apiKey}&${limit}${cantidadFotos}`);
+        setImagenes([...imagenes, ...response.data]);//... SPREAD
+        setCargando(false)
+    }
+
+
     return (
-        <div className='container'>
+        <div 
+            className='container'>
             <Header />        
-            <MuestraImagenes />
+            <div 
+                className='resultados'>
+                {console.log(imagenes)}
+                {imagenes.map(imagen => (
+                    <MuestraImagenes url={imagen.urls.small_s3} key={imagen.id} description={imagen.description} />
+                ))}
+            </div>
         </div>
     )
 }
--- src/components/MuestraImagenes/MuestraImagenes.jsx
+++ src/components/MuestraImagenes/MuestraImagenes.jsx
@@ -1,40 +1,18 @@
 import React, { useState, useEffect } from 'react'
-import { useNavigate, useSearchParams } from 'react-router-dom'
-import axios from 'axios'
-import { BsFillTagFill, BsFillMapFill, BsFillPersonFill, BsFillCameraFill } from "react-icons/bs"
 import './MuestraImagenes.css'
+import { useNavigate, useSearchParams } from 'react-router-dom'
+import { BsFillTagFill, BsFillMapFill, BsFillPersonFill, BsFillCameraFill } from "react-icons/bs"
 import { Loader } from '../Loader/Loader'
 
 
 
-const MuestraImagenes = ({ busqueda }) => {
-    const BASEurl = 'https://api.unsplash.com/';
-    //const apiKey = process.env.REACT_APP_ACCESSKEY;
-    const apiKey = 'kGS0Yw08xWCh64TiRy_U421UZRvGQhnmYWYuoIPWAs8'
-    const apiSearch = 'search/photos?query='
-    const apiRandom = 'photos/random'
-    const cantidadFotos = '5'
-    const limit = 'count='
-
-    const [imagenes, setImagenes] = useState([])
-    const [cargando, setCargando] = useState(false)
-    
-    useEffect(() => {
-        const traeImagenes = async () => {
-            setCargando(true)
-            const response = await axios.get(`${BASEurl}${apiRandom}?client_id=${apiKey}&${limit}${cantidadFotos}`);
-            console.log(response)
-            setImagenes(response.data);
-            setCargando(false)
-        }
-        traeImagenes()
-    }, [])
+const MuestraImagenes = ({ url, key, description }) => {
+ 
 
     return (
-        <div 
-            className='resultados'>
-                <Loader />
-            {/*cargando ? <Loader /> : infoImg*/}
+        <div
+            className='card'>
+            <img src={url} key={key} alt={description} />
         </div>
     )
 }
